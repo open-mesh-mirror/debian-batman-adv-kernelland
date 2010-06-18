a3087fa8e58b672e9c5e461436d87c738a096ae7
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-06-19 00:41:40 +0200

    Change get-orig-source.sh to support new versioning scheme

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -2,7 +2,7 @@
 set -e
 
 REV="`dpkg-parsechangelog|grep '^Version: '|sed 's/^Version:\s*\([0-9]*:\)\?\(.*\)-[0-9]*/\2/'`"
-GITREV="`echo ${REV}|grep "~v"|sed 's/.*~\(v.*\)$/\1/'|sed 's/\+/-/g'`"
+GITREV="`echo ${REV}|grep "+v"|sed 's/.*+\(v.*\)$/\1/'|sed 's/\+/-/g'`"
 DIR="`dpkg-parsechangelog|grep '^Source: '|sed 's/^Source:\s*//'`"
 TARNAME="${DIR}_${REV}.orig.tar.gz"
 TRUNK="http://downloads.open-mesh.net/svn/batman/trunk"
