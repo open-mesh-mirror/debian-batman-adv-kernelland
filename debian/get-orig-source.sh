cec5f5a668c4443ff218c85d5e014451e8a111ce
Sven Eckelmann <sven.eckelmann@gmx.de>
2010-09-02 21:37:25 +0200

    Change get-orig-source.sh to support versions from next branch

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -2,19 +2,20 @@
 set -e
 
 REV="`dpkg-parsechangelog|grep '^Version: '|sed 's/^Version:\s*\([0-9]*:\)\?\(.*\)-[0-9]*/\2/'`"
-GITREV="`echo ${REV}|grep "+v"|sed 's/.*+\(v.*\)$/\1/'|sed 's/\+/-/g'`"
+GITREV="`echo v${REV}|grep "[~+]g"|sed 's/[~+]/-/g'`"
 DIR="`dpkg-parsechangelog|grep '^Source: '|sed 's/^Source:\s*//'`"
 TARNAME="${DIR}_${REV}.orig.tar.gz"
-TRUNK="http://downloads.open-mesh.net/svn/batman/trunk"
+UPSTREAM="`echo ${DIR}|sed 's/\-kernelland$//'`"
+SNAPSHOT="http://git.open-mesh.org/?p=${UPSTREAM}.git;a=snapshot;h=${GITREV};sf=tgz"
 
 # try to download source package
 if [ ! -s "${TARNAME}" ]; then
-	if [ -z "$GITREV" ]; then
+	if [ -z "${GITREV}" ]; then
 		uscan --verbose --force-download --download-version "${REV}" --destdir ..
 	else
 		TMPGZ="`mktemp -t`"
 		TMPTAR="`mktemp -t`"
-		wget -q "http://git.open-mesh.net/snapshot/${GITREV}/" -O "${TMPGZ}"
+		wget -q "${SNAPSHOT}" -O "${TMPGZ}"
 		zcat "${TMPGZ}" > "${TMPTAR}"
 		rm -f "${TMPGZ}"
 		gzip -n -m -f -c "${TMPTAR}" > "../${TARNAME}"
